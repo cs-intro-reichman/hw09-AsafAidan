1f3ff897d02122c584f1e02c7a821ca91c10d72a
Asaf Aidan <asaf.aidan@post.runi.ac.il>
2026-01-20 13:26:40 +0200

    complete assignment

--- List.java
+++ List.java
@@ -137,16 +137,14 @@ public class List {
 
     /** Returns an iterator over the elements in this list, starting at the given index. */
     public ListIterator listIterator(int index) {
-	    // If the list is empty, there is nothing to iterate   
+	    // checkes if the list is empty, if so, exit the function
 	    if (size == 0) return null;
-	    // Gets the element in position index of this list
 	    Node current = first;
 	    int i = 0;
         while (i < index) {
             current = current.next;
             i++;
         }
-        // Returns an iterator that starts in that element
 	    return new ListIterator(current);
     }
 }
